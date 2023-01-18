6e231522e30593ceeba9ea886a800b951705f007
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-18 23:10:03 +0100

    Created logo that change color

---
+++ src/lib/header.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Logo from './assets/svg/logo.svelte';
+	const color1 = '#000';
+	const color2 = '#9747ff';
+	let color = '#000';
+
+	function changeColor() {
+		if (color === color1) {
+			color = color2;
+		} else {
+			color = color1;
+		}
+	}
+
+</script>
+
+<header class="header">
+	<a 
+	href="/"
+	on:mouseenter={changeColor} 
+	on:mouseleave={changeColor}>
+		<Logo
+		 	{color}
+		/>
+	</a>
+
+</header>
+
+<style>
+	.header {
+		grid-column: 1 / 13;
+	}
+</style>
