b389f8e2217c36792fab18d7e848e3c57fe97979
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-18 21:50:58 +0100

    Added variables

---
+++ src/global/css/variables.svelte
@@ -0,0 +1,14 @@
+<style>
+	:root {
+		--primary-color: #ffffff,
+		--secondary-color: #000000,
+		--tertiary-color: #9747ff,
+		--quaternary-color: #e7e7e7,
+
+		--font-body-text: 2rem 'Times New Roman';
+		--font-body-text-uppercase: 2rem Uppercase 'Times New Roman';
+		--font-large-text: 4rem/4.8rem 'neue-haas-grotesk-text', sans-serif;
+		--font-small-text: 1rem;
+		--font-small-text-uppercase: 1rem/1.2rem Uppercase 'Times New Roman';
+	}
+</style>
