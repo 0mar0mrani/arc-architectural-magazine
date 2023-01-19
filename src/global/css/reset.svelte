5d7dd17c985c26719a1eb1720939dbabebde87c5
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-19 19:30:13 +0100

    Added resets for svg, button, ul, a

--- src/global/css/reset.svelte
+++ src/global/css/reset.svelte
@@ -4,4 +4,24 @@
 		margin: 0;
 		box-sizing: border-box;
 	}
+
+	:global(svg) {
+		width: 100%;
+		height: auto;
+	}
+
+	:global(button) {
+		background-color: inherit;
+		border: none;
+		cursor: pointer;
+	}
+
+	:global(ul) {
+		list-style: none;
+	}
+
+	:global(a) {
+		text-decoration: none;
+		color: inherit;
+	}
 </style>
