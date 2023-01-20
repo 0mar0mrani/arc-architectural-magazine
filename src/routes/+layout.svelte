4f23199b839a76c5d2633f48fd06e6b5f3413639
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-20 12:45:17 +0100

    Made the header a part of the layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../global/css/main.svelte'
+	import Header from '../lib/header.svelte';
+</script>
+
+<div class="layout">
+	<Header/>
+
+	<slot></slot>
+</div>
+
+<style>
+	.layout {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		margin: 1rem;
+	}
+</style>
