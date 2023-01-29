b8d7e25da3561680296d53524e14f324d49a264a
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-29 23:22:03 +0100

    Added ad section

--- src/lib/Articles.svelte
+++ src/lib/Articles.svelte
@@ -1,64 +1,7 @@
 <script>
 	import ArticleCard from '$lib/ArticleCard.svelte';
 
-	const articles = [
-		{
-			header: 'Anhalt University of Applied Sciences',
-			title: 'Dessau Summer School of Architecture',
-			image: 'https://i.postimg.cc/B6v5j0qF/alan-rostovtev.jpg',
-			alt: 'White wall',
-			link: '/article',
-			gridClass: 'article-overview__full-width order1',
-		},
-		{
-			header: 'ArkDes',
-			title: 'Joar Nango: Girjegumpi',
-			image: 'https://i.postimg.cc/9XKPQKs4/mohammad-mohsen-rohani.jpg',
-			alt: 'White building',
-			link: '/article',
-			gridClass: 'article-overview__span4 order2',
-		},
-		{
-			header: 'Sabu Kohso',
-			title: 'Nuclear Recursivity and Seismic Awakening',
-			image: 'https://i.postimg.cc/FHQjjcrN/albert-stoynov.jpg',
-			alt: 'Angled building',
-			link: '/article',
-			gridClass: 'article-overview__span5x2 order1',
-		},
-		{
-			header: 'Bauhaus Dessau Foundation',
-			title: 'Bauhaus Lab 2023: Concrete Antarctic',
-			image: 'https://i.postimg.cc/HxPw5hxh/brice-cooper.jpg',
-			alt: 'Building with bridge',
-			link: '/article',
-			gridClass: 'article-overview__span4',
-		},
-		{
-			header: 'Radiowaves Collective',
-			title: 'Mothering a Movement: Notes from India’s Longest Anti-Nuclear Struggle',
-			image: 'https://i.postimg.cc/gkhVX4Pg/op23.jpg',
-			alt: 'Glass building',
-			link: '/article',
-			gridClass: 'article-overview__full-width',
-		},
-		{
-			header: 'Livia Krohn Miller',
-			title: 'Ten Thousand Years of Isolation',
-			image: 'https://i.postimg.cc/Hk4b3xn5/refargotohp.jpg',
-			alt: 'White building',
-			link: '/article',
-			gridClass: 'article-overview__span5',
-		},
-		{
-			header: 'AA editorial',
-			title: 'The Avant-Garde Museum',
-			image: 'https://i.postimg.cc/GtWvgTvP/tingfeng-xia.jpg',
-			alt: 'Bridge',
-			link: '/article',
-			gridClass: 'article-overview__span4',
-		},
-	]
+	export let articles;
 </script>
 
 {#each articles as article}
---
+++ src/lib/ad.svelte
@@ -0,0 +1,65 @@
+<section class="ads">
+	<div class="ads__ad">
+		<p>(ad placeholder)</p>
+
+		<p>4:3</p>
+	</div>
+
+	<div class="ads__ad">
+		<p>(ad placeholder)</p>
+
+		<p>4:3</p>
+	</div>
+
+	<div class="ads__ad">
+		<p>(ad placeholder)</p>
+
+		<p>4:3</p>
+	</div>
+
+	<div class="ads__ad">
+		<p>(ad placeholder)</p>
+
+		<p>4:3</p>
+	</div>
+</section>
+
+<style>
+	.ads {
+		grid-column: 1 / 10;
+		display: flex;
+		flex-direction: column;
+		font: var(--font-large-text);
+		color: var(--primary-color);
+		background: url('https://i.postimg.cc/mkmDF6v3/ad-background.jpg');
+		background-size: contain;
+	}
+	
+	.ads__ad {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-color: var(--tertiary-color);
+		aspect-ratio: 4 / 3;
+		margin: 0 1rem 1rem 1rem;
+	}
+
+	.ads__ad:first-child {
+		margin-top: 1rem;
+	}
+
+	@media only screen and (min-width: 800px) {
+		.ads {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			gap: 1rem;
+			padding: 1rem;
+		}
+		
+		.ads__ad,
+		.ads__ad:first-child {
+			margin: 0;
+		}
+	}
+</style>
