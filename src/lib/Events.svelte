e6f5e6c7f7b4347f8c75f0d71124981169f54fec
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-29 19:42:30 +0100

    Made filter buttons functional

--- src/lib/Events.svelte
+++ src/lib/Events.svelte
@@ -44,20 +44,107 @@
 			date: 'March 4 18:00–20:00',
 			title: 'Monumental love – Art historian Marit Paasche on Hannah Ryggen'
 		},
+		{
+			category: 'Lectures',
+			date: 'January 20 12:00-14:00',
+			title: 'Conversation: Sami Sami sacred mountains and sacrificial stones'
+		},
+		{
+			category: 'Exhibitions',
+			date: 'February 5 16:00–17:00',
+			title: 'Free guided tour in Kunna Guanna Concha'
+		},
+		{
+			category: 'Exhibitions',
+			date: 'February 12 18:00',
+			title: 'Exhibition opening: Metahaven and Susanne M. Winterling'
+		},
+		{
+			category: 'Open Seminars',
+			date: 'March 4 18:00–20:00',
+			title: 'Monumental love – Art historian Marit Paasche on Hannah Ryggen'
+		},
+		{
+			category: 'Lectures',
+			date: 'January 20 12:00-14:00',
+			title: 'Conversation: Sami Sami sacred mountains and sacrificial stones'
+		},
+		{
+			category: 'Exhibitions',
+			date: 'February 5 16:00–17:00',
+			title: 'Free guided tour in Kunna Guanna Concha'
+		},
+		{
+			category: 'Exhibitions',
+			date: 'February 12 18:00',
+			title: 'Exhibition opening: Metahaven and Susanne M. Winterling'
+		},
+		{
+			category: 'Open Seminars',
+			date: 'March 4 18:00–20:00',
+			title: 'Monumental love – Art historian Marit Paasche on Hannah Ryggen'
+		},
 	]
+
+	let currentFilter = null;
+	let filteredEvents = filterEvents();
+
+	function handleEventButtonClick(event) {
+		setCurrentFilter(event);
+		filteredEvents = filterEvents();
+	}
+
+	function setCurrentFilter(event) {
+		const buttonName = event.currentTarget.innerHTML;
+
+		if (buttonName === currentFilter) {
+			currentFilter = null;
+		} else {
+			currentFilter = buttonName;
+		}
+	}
+
+	function filterEvents() {
+		const eventsCopy = [...events];
+
+		if (currentFilter === null) {
+			return events;
+		} else {
+			return eventsCopy.filter(event => event.category.toLowerCase() === currentFilter)
+		}
+	}
 </script>
 
-<div class="header__events">	
-	{#each events as event}
-		<div class="header__event">
-			<p class="header__event-category">{event.category}</p>
-			<p class="header__event-date">{event.date}</p>
-			<p class="header__event-title">{event.title}</p>
+<div class="header__events-container">	
+	<div>
+		<p>UPCOMING EVENTS</p>
+	
+		<div class="header__event-buttons">
+			<button class="header__event-button" on:click={handleEventButtonClick}>lectures</button>
+	
+			<button class="header__event-button" on:click={handleEventButtonClick}>exhibitions</button>
+	
+			<button class="header__event-button" on:click={handleEventButtonClick}>open seminars</button>
 		</div>
-	{/each}
+	</div>
+
+	<div class="header__events">
+		{#each filteredEvents as event}
+			<div class="header__event">
+				<p class="header__event-category">{event.category}</p>
+				<p class="header__event-date">{event.date}</p>
+				<p class="header__event-title">{event.title}</p>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.header__event-button {
+		color: inherit;
+		text-transform: uppercase;
+	}
+
 	.header__events {
 		display: flex;
 		flex-direction: column;
