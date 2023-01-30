1e03581763f2783bd162c9134ab8c3d6f01af0f6
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-30 21:33:33 +0100

    Added search svg

---
+++ src/lib/assets/svg/Search.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let isSearchOpen;
+</script>
+
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" class:open={isSearchOpen}>
+	<circle cx="11.5" cy="11.5" r="9.5" stroke="black" stroke-width="4"/>
+	<path d="M19 19L30 30" stroke="black" stroke-width="4"/>
+</svg>
+	
+
+<style>
+	circle,
+	path {
+		stroke: var(--secondary-color);
+	}
+
+	.open circle,
+	.open path {
+		stroke: var(--tertiary-color);
+	}
+</style>
+	
