8523170da3ba1bce9299731ae1725bebd8da8e94
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-02-02 00:38:32 +0100

    Made dropdown menu height dynamic

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -77,9 +77,12 @@
 	
 	const desktopWidth = 800;
 
-	let logoEl;
-	let headerMenuEl;
 	let headerEl;
+	let menuEl;
+	let menuTopEl;
+	let dropDownMenuEl;
+	let dropDownSearchEl;
+	let logoEl;
 	let inputEl;
 
 	onMount(() => {
@@ -93,6 +96,11 @@
 		isSearchOpen = false;
 		searchString = '';
 		isMenuOpen = !isMenuOpen;
+		
+		if (isMenuOpen) {
+			setDropDownHeight();
+			console.log('yooo');
+		}
 	}
 
 	function handleSearchButtonClick() {
@@ -121,6 +129,7 @@
 	function handleWindowScroll() {
 		setIsSticky();
 		setMarginToHeader();
+		setDropDownHeight();
 	}
 
 	function handleResultClick() {
@@ -192,12 +201,23 @@
 	}
 
 	function setMarginToHeader() {
-		const menuHeight = headerMenuEl.offsetHeight;
+		const menuTopHeight = menuTopEl.offsetHeight;
 
 		if (isSticky) {
-			headerEl.style.marginBottom = `${menuHeight}px`;
+			headerEl.style.marginBottom = `${menuTopHeight}px`;
 		} else {
 			headerEl.style.marginBottom = '';
+		}
+	}
+
+	function setDropDownHeight() {
+		if (isMenuOpen || isSearchOpen) {
+			const headerMenuRelativeToView = menuEl.getBoundingClientRect().top;
+			const menuTopHeight = menuTopEl.offsetHeight;
+			const dynamicHeight = `calc(100dvh - ${menuTopHeight}px - ${headerMenuRelativeToView}px)`
+	
+			dropDownMenuEl.style.maxHeight = dynamicHeight;
+			dropDownSearchEl.style.maxHeight = dynamicHeight;
 		}
 	}
 </script>
@@ -209,8 +229,8 @@
 		<Logo/>
 	</a>
 
-	<div class={`header__menu ${isSticky ? 'header__menu--fixed' : ''}`} bind:this={headerMenuEl}>
-		<div class="header__menu-name-button-container">
+	<div class={`header__menu ${isSticky ? 'header__menu--fixed' : ''}`} bind:this={menuEl}>
+		<div class="header__menu-name-button-container" bind:this={menuTopEl}>
 			<a href="/">
 				<h1 class="header__name">Arc Architectural</h1>
 			</a>
@@ -244,69 +264,65 @@
 				</button>
 			</div>
 
-			{#if searchString !== ''}
-				<ul class="header__search-results">
-					{#each filteredArticles as article}
-						<li class="header__search-item">
-							<a href={article.link} on:click={handleResultClick}>
-								<div class="header__search-item-image">
-									<img src={article.image} alt={article.alt}>
-								</div>
-								
-								<div  class="header__search-item-title">
-									<p>{article.title}</p>
-								</div>
-							</a>
-						</li>
-					{/each}
-				</ul>
-			{/if}
+			<ul class="header__search-results" class:hidden={searchString === ''} bind:this={dropDownSearchEl}>
+				{#each filteredArticles as article}
+					<li class="header__search-item">
+						<a href={article.link} on:click={handleResultClick}>
+							<div class="header__search-item-image">
+								<img src={article.image} alt={article.alt}>
+							</div>
+							
+							<div  class="header__search-item-title">
+								<p>{article.title}</p>
+							</div>
+						</a>
+					</li>
+				{/each}
+			</ul>
 		</div>
 
-		<div class="header__main-container">
-			{#if isMenuOpen}
-				<div class="header__about">
-					<p>
-						AA was founded in 1847 with the aspiration of ‘promoting and affording facilities for the study of architecture for the public benefit’. Likeness it them very firmament without created also bring a one Whose she'd in. Sea unto created every together together. For that wherein. Kind sea earth them was place. Sixth signs saying after replenish multiply. Female every unto beginning appear moving tree the bearing own.
-					</p>
-				</div>
+		<div class="header__main-container" class:hidden={!isMenuOpen} bind:this={dropDownMenuEl}>
+			<div class="header__about">
+				<p>
+					AA was founded in 1847 with the aspiration of ‘promoting and affording facilities for the study of architecture for the public benefit’. Likeness it them very firmament without created also bring a one Whose she'd in. Sea unto created every together together. For that wherein. Kind sea earth them was place. Sixth signs saying after replenish multiply. Female every unto beginning appear moving tree the bearing own.
+				</p>
+			</div>
 
-				<nav class="header__navigation">
-					<ul>
-						<li>
-							<a href="/information" on:click={handleMenuNavClick}>
-								Announcements
-							</a>
-						</li>
+			<nav class="header__navigation">
+				<ul>
+					<li>
+						<a href="/information" on:click={handleMenuNavClick}>
+							Announcements
+						</a>
+					</li>
 
-						<li>
-							<a href="/information" on:click={handleMenuNavClick}>
-								Journal
-							</a>
-						</li>
+					<li>
+						<a href="/information" on:click={handleMenuNavClick}>
+							Journal
+						</a>
+					</li>
 
-						<li>
-							<a href="/information" on:click={handleMenuNavClick}>
-								Reviews
-							</a>
-						</li>
+					<li>
+						<a href="/information" on:click={handleMenuNavClick}>
+							Reviews
+						</a>
+					</li>
 
-						<li>
-							<a href="/information" on:click={handleMenuNavClick}>
-								Books
-							</a>
-						</li>
+					<li>
+						<a href="/information" on:click={handleMenuNavClick}>
+							Books
+						</a>
+					</li>
 
-						<li>
-							<a href="/information" on:click={handleMenuNavClick}>
-								Information
-							</a>
-						</li>
-					</ul>
-				</nav>
+					<li>
+						<a href="/information" on:click={handleMenuNavClick}>
+							Information
+						</a>
+					</li>
+				</ul>
+			</nav>
 
-				<Events/>
-			{/if}
+			<Events/>
 		</div>
 	</div>
 </header>
@@ -322,7 +338,7 @@
 		flex-direction: column;
 		justify-content: space-between;
 		position: relative;
-		transform: scale(1.001);
+		/* transform: scale(1.001); */
 	}
 
 	.header__menu-name-button-container {
@@ -335,7 +351,7 @@
 
 	.header__main-container {
 		overflow-y: scroll;
-		max-height: calc(100dvh - 9rem);
+		/* max-height: calc(100dvh - 9rem); */
 		background-color: var(--primary-color);
 	}
 	
@@ -410,7 +426,7 @@
 		max-height: calc(100dvh - 8.8rem);
 		overflow-y: scroll;
 		background-color: var(--primary-color);
-		transform: scale(1.005);
+		/* transform: scale(1.005); */
 		display: flex;
 		flex-direction: column;
 	}
@@ -446,7 +462,6 @@
 	.header__about {
 		font: var(--font-body-text);
 		padding: 2rem 0;
-		border-top: solid 1px var(--secondary-color);
 		border-bottom: solid 1px var(--secondary-color);
 	}
 
@@ -456,6 +471,10 @@
 		background-color: var(--primary-color);
 		padding: 2rem 0;
 		font-size: 4rem;
+	}
+
+	.hidden { 
+		display: none !important;
 	}
 
 	@media screen and (min-width: 800px) {
